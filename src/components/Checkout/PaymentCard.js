2a59b6e22b710f7feeededb53829af0ef2d514f4
Romana-Aijaz <romanaaijaz99@gmail.com>
2023-10-24 17:02:53 +0500

    Address screen

--- src/components/Checkout/PaymentCard.js
+++ src/components/Checkout/PaymentCard.js
@@ -50,6 +50,7 @@ function PaymentCard({
   loading,
   calculateTotal,
 }) {
+  console.log(validateOrder)
   const theme = useTheme();
   const classes = useStyles();
   const { cart } = useContext(UserContext);
